0bf31a5fcbe40f0bb3848354928264eb97baa9a0
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2021-01-14 16:37:42 +0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,25 @@
 (function ($) {
     "use strict";
 
-//////////////////////// Window On Load //////////////////
-     $(window).on('load', function () {
+    //////////////////////// Window On Load //////////////////
+    $(window).on('load', function () {
         $('#preloader').delay(350).fadeOut('slow');
         $('body').delay(350).css({ 'overflow': 'visible' });
     })
 
 
-/////////////////////// Loader /////////////////////
+    /////////////////////// Loader /////////////////////
     var angle = 0;
     setInterval(function () {
 
         $(".se-pre-con img")
-                .css('-webkit-transform', 'rotate(' + angle + 'deg)')
-                .css('-moz-transform', 'rotate(' + angle + 'deg)')
-                .css('-ms-transform', 'rotate(' + angle + 'deg)');
+            .css('-webkit-transform', 'rotate(' + angle + 'deg)')
+            .css('-moz-transform', 'rotate(' + angle + 'deg)')
+            .css('-ms-transform', 'rotate(' + angle + 'deg)');
         angle++;
         angle++;
         angle++;
     }, 10);
-
-    $('.background-image-maker').each(function () {
-        var imgURL = $(this).next('.holder-image').find('img').attr('src');
-        $(this).css('background-image', 'url(' + imgURL + ')');
-    });
 
     $('#header-fix .nav li a[href^="#"]:not([href="#"])').on('click', function (event) {
         var $anchor = $(this);
@@ -39,59 +34,59 @@
     });
 
 
-    $("body").scrollspy({target: ".scrollspy", offset: 100});
+    $("body").scrollspy({ target: ".scrollspy", offset: 100 });
 
 })(jQuery);
 
 function sendContact() {
-    var valid;	
+    var valid;
     valid = validateContact();
-    if(valid) {
+    if (valid) {
         jQuery.ajax({
             url: "contact_mail.php",
-            data:'userName='+$("#userName").val()+'&userEmail='+
-            $("#userEmail").val()+'&subject='+
-            $("#subject").val()+'&content='+
-            $(content).val(),
+            data: 'userName=' + $("#userName").val() + '&userEmail=' +
+                $("#userEmail").val() + '&subject=' +
+                $("#subject").val() + '&content=' +
+                $(content).val(),
             type: "POST",
-            success:function(data){
+            success: function (data) {
                 $("#mail-status").html(data);
                 $('#userEmail').val('');
-                 $('#subject').val('');
-                  $('#userName').val('');
-                   $('#content').val('');
+                $('#subject').val('');
+                $('#userName').val('');
+                $('#content').val('');
             },
-            error:function (){}
+            error: function () { }
         });
     }
 }
 function validateContact() {
-    var valid = true;	
-    $(".demoInputBox").css('background-color','');
+    var valid = true;
+    $(".demoInputBox").css('background-color', '');
     $(".info").html('');
-    if(!$("#userName").val()) {
-       
-        $("#userName").css('border','1px solid red');
+    if (!$("#userName").val()) {
+
+        $("#userName").css('border', '1px solid red');
         valid = false;
     }
-    if(!$("#userEmail").val()) {
-      
-        $("#userEmail").css('border','1px solid red');
+    if (!$("#userEmail").val()) {
+
+        $("#userEmail").css('border', '1px solid red');
         valid = false;
     }
-    if(!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
-      
-        $("#userEmail").css('border','1px solid red');
+    if (!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+
+        $("#userEmail").css('border', '1px solid red');
         valid = false;
     }
-    if(!$("#subject").val()) {
-      
-        $("#subject").css('border','1px solid red');
+    if (!$("#subject").val()) {
+
+        $("#subject").css('border', '1px solid red');
         valid = false;
     }
-    if(!$("#content").val()) {
-        
-        $("#content").css('border','1px solid red');
+    if (!$("#content").val()) {
+
+        $("#content").css('border', '1px solid red');
         valid = false;
     }
     return valid;
