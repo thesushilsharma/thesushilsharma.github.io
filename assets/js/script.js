7cabce971e1c6045ee56b089e6d4e1f6a9927630
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2021-08-31 23:33:16 +0400

    scrollspy

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var $root = $("html, body");
 $(document).ready(function () {
   "use strict";
   typedJS();
+  $("body").scrollspy({ target: ".scrollspy", offset: 100 });
 });
 
 function menuToggler() {
