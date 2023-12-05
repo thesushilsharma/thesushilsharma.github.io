1add44536e7fa257570b921b87d7d7d37fe9b3f1
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2023-12-05 23:38:05 +0400

    updating

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -25,7 +25,7 @@
                 <div>
                     <label for="subject"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                    <input type="text" id="subject" placeholder="Let us know how we can help"
+                    <input type="text" id="subject" placeholder="Feel free to let me know how I can help."
                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         required>
                 </div>
@@ -47,3 +47,8 @@
         </div>
     </section>
 </template>
+
+<script setup>
+
+
+</script>
