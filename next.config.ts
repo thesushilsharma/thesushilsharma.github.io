b7f1064d45cb4fe00d061a2b434947a1689c3f6a
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-10-13 01:05:47 +0400

    updating next js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images: {
+    remotePatterns: [new URL('https://placehold.co/600x400.png')],
+  },
 };
 
 export default nextConfig;
