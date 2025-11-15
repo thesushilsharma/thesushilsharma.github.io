abc4f117c4879cb47adb87b25d7673070ba68c3a
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-11-16 03:26:22 +0400

    adding react compiler

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+ reactCompiler: true,
  images: {
     remotePatterns: [new URL('https://placehold.co/600x400.png')],
   },
