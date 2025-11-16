ea1e199c0d2a0262b5b61728167450a65b3a7323
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-11-17 02:15:38 +0400

    Update image remotePatterns in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
  reactCompiler: true,
  images: {
-    remotePatterns: [new URL('https://placehold.co/600x400.png')],
-  },
+   remotePatterns: [
+     {
+       protocol: 'https',
+       hostname: 'images.unsplash.com',
+       pathname: '/**',
+     },
+     {
+       protocol: 'https',
+       hostname: 'placehold.co',
+       pathname: '/**',
+     },
+   ],
+ },
 };
 
 export default nextConfig;
